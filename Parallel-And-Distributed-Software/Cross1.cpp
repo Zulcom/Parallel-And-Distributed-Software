f62528a251b2951a7b653a3b5cdb3eb326390f25
Zulcom <s01@ngs.ru>
2017-09-14 21:41:28 +0700

    Комментарии

--- Parallel-And-Distributed-Software/Cross1.cpp
+++ Parallel-And-Distributed-Software/Cross1.cpp
@@ -1,114 +1,139 @@
-﻿#include <algorithm>
-#include <iostream>
-#include <vector>
-#include "omp.h"
-template <typename T>
-int col_max(const std::vector<std::vector<T> > &matrix, int col, int n)
+﻿#include <algorithm> // место жительства swap()
+#include <iostream> // будем использовать потоковый ввод-вывод
+#include <vector> // матрицы оформим как векторы
+#include "omp.h" // используем openMP
+using namespace std; // чтобы не дописывать пространство имён к векторам, свопам и консоли
+/* Поиск максимального элемента в столбце
+* vector<vector<T> > &matrix матирца
+*  int col столбец
+*   int n размер
+*   возвращает позицию максимального элемента в столбце
+*/
+template <typename T>// шаблон функции, реальная будет создана на этапе компиляции и масимально оптимизированна
+int col_max(const vector<vector<T> > &matrix, int col, int n)
 {
-	T max = std::abs(matrix[col][col]);
-	int maxPos = col;
-#pragma omp parallel
+	T max = abs(matrix[col][col]); // берём за минимальный центровой элемент столбца..
+	int maxPos = col; // ..запомним его индекс
+	#pragma omp parallel // параллелим поиски
 	{
-		T loc_max = max;
-		T loc_max_pos = maxPos;
-#pragma omp for
-		for(int i = col + 1; i < n; ++i)
+		T loc_max = max; // для каждого потока будет локальный максимум..
+		T loc_max_pos = maxPos; // ...и его позиция
+		#pragma omp for // параллельно обрабытываем разные части строки
+		for(int i = col + 1; i < n; ++i) // мы уже запомнили центровой, поэтому проверяем всё от него до конца
 		{
-			T element = std::abs(matrix[i][col]);
-			if(element > loc_max)
+			T element = abs(matrix[i][col]); // запомним текущий элемент
+			if(element > loc_max) // если он больше предыдущего максимуму
 			{
-				loc_max = element;
-				loc_max_pos = i;
+				loc_max = element; // меняем этот максимум
+				loc_max_pos = i; // и запомненную позицию
 			}
 		}
-#pragma omp critical
+		#pragma omp critical // потоки не должны переписывать общий максимум, иначе из-за эффекта гонки будет беда. Помечаем область критической
 		{
-			if(max < loc_max)
+			if(max < loc_max) // проверяем, больше ли найденные максимум чем тот максимум, который был найден в других потоках
 			{
-				max = loc_max;
-				maxPos = loc_max_pos;
+				max = loc_max; // если да, то меняем "глобальный" максимум
+				maxPos = loc_max_pos; // и запомненную позицию
 			}
 		}
 	}
-	return maxPos;
+	return maxPos; // вернём позицию максимального
 }
-template <typename T>
-int triangulation(std::vector<std::vector<T> > &matrix, int n)
+/* Триангуляция матрицы (приведение к "треугольному")
+ * Треуго́льная матрица — матрица, у которой все элементы, стоящие ниже (или выше) главной диагонали, равны нулю.
+* vector<vector<T> > &matrix матрица для триангулцияя
+* int n её размерность
+* Возвращает количество совершенных перестановок
+*/
+template <typename T>// шаблон функции, реальная будет создана на этапе компиляции и масимально оптимизированна
+int triangulation(vector<vector<T> > &matrix, int n)
 {
-	unsigned int swapCount = 0;
-	if(0 == n)
-		return swapCount;
-	const int num_cols = matrix[0].size();
-	for(int i = 0; i < n - 1; ++i)
+	unsigned int swapCount = 0; // здесь будет счётчик количества перестановок. Очевидно, что он только положительный.
+	if(0 == n) // если матрица пуста...
+		return swapCount; // ...переставлять нечего
+	const int num_cols = matrix[0].size(); // запомним количество столбцов
+	for(int i = 0; i < n - 1; ++i) // пробегаем по всем строкам кроме поледней, потому что мы не сможем переставить последнюю+1
 	{
-		unsigned int imax = col_max(matrix, i, n);
-		if(i != imax)
+		unsigned int imax = col_max(matrix, i, n); // поиск номера строки содержащей максимальный по модулю элемент столбца с номером i
+		if(i != imax) // если текущая строка не максимальна
 		{
-			swap(matrix[i], matrix[imax]);
-			++swapCount;
+			swap(matrix[i], matrix[imax]); // ставим ёё его место максимальной
+			++swapCount; // накидываем счётчик перестановок
 		}
-		for(int j = i + 1; j < n; ++j)
+		#pragma omp parallel for // параллелим эквивалентные преобразования
+		for(int j = i + 1; j < n; ++j) // пробегаем по всем элементам строки
 		{
-			T mul = -matrix[j][i] / matrix[i][i];
-			for(int k = i; k < num_cols; ++k)
+			T mul = -matrix[j][i] / matrix[i][i]; // вычисляем число, на которое нужно домножить
+			for(int k = i; k < num_cols; ++k)  // бежим по столбцам снова
 			{
-				matrix[j][k] += matrix[i][k] * mul;
+				matrix[j][k] += matrix[i][k] * mul; // домножаем элемент на число и складыываем строки
 			}
 		}
 	}
-	return swapCount;
+	return swapCount; // возвращаем кол-во перестановок
 }
-template <typename T>
-T gauss_determinant(std::vector<std::vector<T> > &matrix, int n)
+/* Поиск определителя методом Гаусса
+ * vector<vector<T> > &matrix матрица для поиска
+ * int n её размерность
+ * Возвращает определитель
+ */
+template <typename T>// шаблон функции, реальная будет создана на этапе компиляции и масимально оптимизированна
+T gauss_determinant(vector<vector<T> > &matrix, int n)
 {
-	unsigned int swapCount = triangulation(matrix, n);
-	T determinanit = 1;
-	if(swapCount % 2 == 1)
-		determinanit = -1;
-	for(int i = 0; i < n; ++i)
+	unsigned int swapCount = triangulation(matrix, n); // ищем количество перестановок строк и делаем триангуляцию матрицы
+	T determinanit = 1; // объявляем определитель как 1, на случай если перестановок не будет
+	if(swapCount % 2 == 1) // если количество перестановок нечётное...
+		determinanit = -1; // ..очевидно, что определитель будет отрицательный, поскольку при каждой перестановке он меняет знак
+	for(int i = 0; i < n; ++i) // не параллелим этот цикл поскольку на быстродействии это не скажется - только память копиями забьём
 	{
-		determinanit *= matrix[i][i];
+		determinanit *= matrix[i][i]; // считаем произведение элементов на главной диагонали
 	}
-	return determinanit;
+	return determinanit; // возвращаем их
 }
 
-template <typename T>
-std::vector<T> cramer_solving(std::vector<std::vector<T> > &matrix,
-	std::vector<T> &free_term_column, int n)
+/* Решение СЛАУ методом Крамера
+ * vector<vector<T> > &matrix - матрица неизвестых
+ *  vector<T> &free_term_column - слобец свободных членов
+ * int n - размерность
+ * Возвращает вектор, содержащий решения к матрице
+ */
+template <typename T> // шаблон функции, реальная будет создана на этапе компиляции и масимально оптимизированна
+vector<T> cramer_solving(vector<vector<T> > &matrix,vector<T> &free_term_column, int n)
 {
-	T mainDet = gauss_determinant(matrix, n);
-	if(std::abs(mainDet) < 0.0001)
-		throw 20;
-	std::vector<T> solution(n);
-#pragma omp parallel
+	T mainDet = gauss_determinant(matrix, n); // вычисляем определитель матрицы неизвестных
+	if(abs(mainDet) < 0.0001) // если он не подходит под решение по правилу Крамера
+		return nullptr; //возвращаем пустой указатель
+	vector<T> solution(n); // создаем вектор решений
+	#pragma omp parallel // просим openMP сделать этот блок параллельно
 	{
-		std::vector<std::vector<T> > private_matrix = matrix;
-#pragma omp for
-		for(int i = 0; i < n; ++i)
+		vector<vector<T> > private_matrix = matrix; // делаем копию матрицы, чтобы избежать работы в критической секции и эффекта гонки
+		#pragma omp for //параллелим цикл
+		for(int i = 0; i < n; ++i) //бежим по всем столбцам
 		{
-			std::swap(matrix[i], free_term_column);
-			solution[i] = gauss_determinant(private_matrix, n) / mainDet;
-			std::swap(matrix[i], free_term_column);
+			swap(matrix[i], free_term_column); // заменяем столбец неизвестных и столбец i в матрице
+			solution[i] = gauss_determinant(private_matrix, n) / mainDet; // изем определитель и делим на главный, результат в вектор
+			swap(matrix[i], free_term_column); // заменяем обратно чтобы не путаться 
 		}
 	}
-	return solution;
+	return solution; // возвращаем решения
 }
-int main()
+int main() // точка входа
 {
-	for(int n = 50; n < 350; n += 50)
+	for(int n = 50; n < 350; n += 50) // основной цикл
 	{
-		std::vector<std::vector<double> > matrix(n);
-		for(int i = 0; i < n; ++i)
+		vector<vector<double> > matrix(n); // матрица, построенная на векторах цифр с плавующей точкой
+		for(int i = 0; i < n; ++i) // заполнение массива - строки
 		{
-			matrix[i].resize(n);
-			for(int j = 0; j < n; ++j)
-				matrix[i][j] = rand();
+			matrix[i].resize(n); // задаём новый размер кажой строке
+			for(int j = 0; j < n; ++j) // заполнение массива - столбцы
+				matrix[i][j] = rand(); // заполяем элемент M_ij случайным числом
 		}
-		std::vector<double> column(n);
-		for(int j = 0; j < n; ++j)
-			column[j] = rand();
-		double start_time = omp_get_wtime();
-		std::vector<double> solution = cramer_solving(matrix, column, n);
-		std::cout << n << " " << omp_get_wtime() - start_time << std::endl;
+		vector<double> column(n); // создаем столбец свободных членов
+		for(int j = 0; j < n; ++j) //заполняем его - строки
+			column[j] = rand(); // заполняем элемент Column_j случайным числом
+		double start_time = omp_get_wtime(); //запоминаем время
+		vector<double> solution = cramer_solving(matrix, column, n); // запоминаем вектор корней СЛАУ, полученный из автоматически оптимизированной функции cramer_solving,
+		//которая принимает матрицу для поиска, столбец свободных членов, размерность матрицы (без столбца св. членов)
+		cout << n << " " << omp_get_wtime() - start_time << endl; // выводим в консоль размерность и затраченное на вычисление время
 	}
 }
