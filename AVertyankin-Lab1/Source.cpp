2535e13f00703304a44ea7fcf40468c9d2420e1a
Zulcom <s01@ngs.ru>
2017-10-05 11:51:58 +0700

    recursive lab1

--- AVertyankin-Lab1/Source.cpp
+++ AVertyankin-Lab1/Source.cpp
@@ -23,6 +23,9 @@ b[i]=(a[i]+c[i])/2, i=0..N
 #include <omp.h>
 #include <fstream>
 using namespace std;
+double bi(int i,int x,int n) {
+	return i == 0 ? x / n : (bi(i - 1, x, n) + n/i)/x;
+}
 void calculate(int n,int x,int k,bool needPrint) {
 	double * a = new double[n];
 	double * b = new double[n];
@@ -31,24 +34,42 @@ void calculate(int n,int x,int k,bool needPrint) {
 	a[0] = x;
 	b[0] = x/n;
 	ofstream out("out.txt");
-#pragma omp parallel num_threads(k)
+#pragma omp parallel num_threads(k> n ? n: k)
 	{
-		out << "Parallel section for calculate A and B, num of threads - " << omp_get_num_threads() << endl;
-		for(int i = 1; i < n; i++)
+
+		int m = ceil(n / static_cast<double>(omp_get_num_threads()));
+		int begin = m*omp_get_thread_num() == 0 ? 1 : m*omp_get_thread_num();
+		int end = begin + m > n ? n : begin + m;
+		for(int i = begin; i < end; i++)
 		{
 			a[i] = x*i + x*x / i;
-			b[i] = (b[i - 1] + n / i) / x;
+			b[i] = bi(i, x, n);
 		}
 	}
-#pragma omp parallel num_threads(k)
+	for(int i = 0; i < n; i++)
 	{
-		out << "Parallel section for calculate C and recalculate B, num of threads - " << omp_get_num_threads() << endl;
-		for(int i = 0; i < n; i++)
+		cout << i<< " " << bi(i, x, n) << " " << b[i] << endl;
+	}
+#pragma omp parallel num_threads(k> n ? n: k)
+	{
+		int m = ceil(n / static_cast<double>(omp_get_num_threads()));
+		int begin = m*omp_get_thread_num();
+		int end = begin + m > n ? n: begin + m;
+		for(int i = begin; i < end; i++)
 		{
-			c[i] = a[i] - b[n - i];
+			c[i] = a[i] - bi(n - 1, x, n);
 			b[i] = (a[i] + c[i]) / 2;
 		}
+#pragma omp  critical
+		out << "Parallel section for calculate A and B, num of threads - " << omp_get_num_threads()
+			<< "Thread" << omp_get_thread_num()
+			<< " m:" << m
+			<< " begin: " << begin
+			<< " end: " << end - 1 << endl;
+
 	}
+
+	
 	if(needPrint)
 	{	
 		out << "a: ";
@@ -73,4 +94,5 @@ int main() {
 	cout << "Print arrays to file?(y/n)";
 	cin >> needPrint;
 		calculate(n,x,k,needPrint == 'y');
+	system("pause.exe");
 }
