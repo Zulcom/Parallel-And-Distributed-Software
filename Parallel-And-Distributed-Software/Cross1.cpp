6dd7d8b8dbe16701869c7fecf0a37f163ebfe784
Zulcom <s01@ngs.ru>
2017-10-07 23:25:25 +0700

    Cross 1 fixes with in from file and out to console

--- Parallel-And-Distributed-Software/Cross1.cpp
+++ Parallel-And-Distributed-Software/Cross1.cpp
@@ -2,6 +2,7 @@
 #include <iostream> // будем использовать потоковый ввод-вывод
 #include <vector> // матрицы оформим как векторы
 #include "omp.h" // используем openMP
+#include <fstream>
 using namespace std; // чтобы не дописывать пространство имён к векторам, свопам и консоли
 /* Поиск максимального элемента в столбце
 * vector<vector<T> > &matrix матирца
@@ -98,40 +99,57 @@ T gauss_determinant(vector<vector<T> > &matrix, int n)
 template <typename T> // шаблон функции, реальная будет создана на этапе компиляции и масимально оптимизированна
 vector<T> cramer_solving(vector<vector<T> > &matrix,vector<T> &free_term_column, int n)
 {
-	T mainDet = gauss_determinant(matrix, n); // вычисляем определитель матрицы неизвестных
+	vector<vector<T> >private_matrix(matrix);
+	
+	T mainDet = gauss_determinant(private_matrix, n); // вычисляем определитель матрицы неизвестных
+
 	if(abs(mainDet) < 0.0001) // если он не подходит под решение по правилу Крамера
 		throw 20; //кидаем исключение
 	vector<T> solution(n); // создаем вектор решений
-	#pragma omp parallel // просим openMP сделать этот блок параллельно
+	#pragma omp parallel private(private_matrix)  // просим openMP сделать этот блок параллельно
 	{
-		vector<vector<T> > private_matrix = matrix; // делаем копию матрицы, чтобы избежать работы в критической секции и эффекта гонки
+		private_matrix = matrix; // делаем копию матрицы, чтобы избежать работы в критической секции и эффекта гонки
 		#pragma omp for //параллелим цикл
 		for(int i = 0; i < n; ++i) //бежим по всем столбцам
 		{
-			swap(matrix[i], free_term_column); // заменяем столбец неизвестных и столбец i в матрице
+			for(int j = 0; j < n; j++)	private_matrix[j][i] = free_term_column[j]; // заменяем столбец на столбец свободных членов
+#pragma omp critical
+			{
+				cout << omp_get_thread_num() << endl;
+				cout << "i:" << i << endl;
+				for(vector<T> k : private_matrix)
+				{
+					for (T k1 : k) cout << k1 << " ";
+					cout << endl;
+				}
+				cout << endl << endl;
+			}
 			solution[i] = gauss_determinant(private_matrix, n) / mainDet; // изем определитель и делим на главный, результат в вектор
-			swap(matrix[i], free_term_column); // заменяем обратно чтобы не путаться 
+		//	swap(private_matrix[i], free_term_column); // заменяем обратно чтобы не путаться 
 		}
 	}
 	return solution; // возвращаем решения
 }
 int main() // точка входа
 {
-	for(int n = 50; n < 350; n += 50) // основной цикл
+	ifstream in("in.txt");
+	int n;
+	in >> n;
+	vector<vector<double> > matrix(n); // матрица, построенная на векторах цифр с плавующей точкой
+	for(int i = 0; i < n; ++i) // заполнение массива - строки
 	{
-		vector<vector<double> > matrix(n); // матрица, построенная на векторах цифр с плавующей точкой
-		for(int i = 0; i < n; ++i) // заполнение массива - строки
-		{
-			matrix[i].resize(n); // задаём новый размер кажой строке
-			for(int j = 0; j < n; ++j) // заполнение массива - столбцы
-				matrix[i][j] = rand(); // заполяем элемент M_ij случайным числом
-		}
-		vector<double> column(n); // создаем столбец свободных членов
-		for(int j = 0; j < n; ++j) //заполняем его - строки
-			column[j] = rand(); // заполняем элемент Column_j случайным числом
-		double start_time = omp_get_wtime(); //запоминаем время
-		vector<double> solution = cramer_solving(matrix, column, n); // запоминаем вектор корней СЛАУ, полученный из автоматически оптимизированной функции cramer_solving,
-		//которая принимает матрицу для поиска, столбец свободных членов, размерность матрицы (без столбца св. членов)
-		cout << n << " " << omp_get_wtime() - start_time << endl; // выводим в консоль размерность и затраченное на вычисление время
+		matrix[i].resize(n); // задаём новый размер кажой строке
+		for(int j = 0; j < n; ++j) // заполнение массива - столбцы
+			in >> matrix[i][j];  // заполяем элемент M_ij случайным числом
 	}
+		
+	vector<double> column(n); // создаем столбец свободных членов
+	for(int j = 0; j < n; ++j) //заполняем его - строки
+		in  >>  column[j]; // заполняем элемент Column_j случайным числом
+	double start_time = omp_get_wtime(); //запоминаем время
+	vector<double> solution = cramer_solving(matrix, column, n); // запоминаем вектор корней СЛАУ, полученный из автоматически оптимизированной функции cramer_solving,
+	//которая принимает матрицу для поиска, столбец свободных членов, размерность матрицы (без столбца св. членов)
+	for(double i : solution) { cout << i << " "; }
+	cout << "Time: "<< omp_get_wtime() - start_time << endl; // выводим в консоль размерность и затраченное на вычисление время
+	system("pause.exe");
 }
