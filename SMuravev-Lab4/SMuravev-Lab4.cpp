3e842934219c104644b54ad77935ea93c9c3d965
Zulcom <s01@ngs.ru>
2017-11-09 15:23:33 +0700

    Lab 4 v1

--- SMuravev-Lab4/SMuravev-Lab4.cpp
+++ SMuravev-Lab4/SMuravev-Lab4.cpp
@@ -7,8 +7,8 @@
 Информацию 3 – вывести на консоль оператора (на экран или по требованию - в файл screen.txt).
 В связи с тем, что на Марсе много коррупционеров, обработка сообщений выполняется несколькими служащими министерства (потоками).
 Кколичество потоков k задается при запуске программы.
- Суммы взяток в местной валюте находятся в массиве а;
- Суммы откатов в местной валюте находятся в массиве b.
+ Суммы взяток в местной валюте находятся в массиве а;
+ Суммы откатов в местной валюте находятся в массиве b.
 За основу работы берутся массивы а и в, инициализированные в лабораторной работе 3 в соответствии с заданием на нее.
 Примечание: если значения, полученные в ЛР1, не подходят для выполнения задания, выражения для их вычисления можно скорректировать.
 Дополнительно:
@@ -34,64 +34,63 @@
 #include <iostream>
 #include <omp.h>
 #include <fstream>
-
-int avgPerc(int* a, int* b, int n)
-	{
+#include <ctime>
+using namespace std;
+int avgPerc(int* a, int* b, int n) {
 	int sum = 0;
 #pragma omp parallel for reduction(+:sum)
 	for (int i = 0; i < n; i++) sum += (100 * b[i] / a[i]);
 	return (double) sum / (double) n;
-	}
+}
 
-int max(int* A, int n)
-	{
-	int max_val= DBL_MAX;
-#pragma omp parallel for 
-	for (int i = 0; i < n; i++) max_val = max_val > A[i] ? max_val : A[i];
+int max(int* A, int n) {
+	int max_val = DBL_MIN;
+	for (int i = 0; i < n; i++) max_val = A[i] > max_val ? A[i] : max_val;
 	return max_val;
-	}
+}
 
-int min(int* A, int n)
-	{
-	int min_val= DBL_MIN;
-#pragma omp parallel for 
-	for (int i = 0; i < n; i++) min_val = min_val > A[i] ? min_val : A[i];
+int min(int* A, int n) {
+	int min_val = DBL_MAX;
+	for (int i = 0; i < n; i++) min_val = A[i] < min_val ? A[i] : min_val;
 	return min_val;
-	}
+}
 
-int main(int argc, char* argv[]){
-	int k, n, x, m;
-	std::cout << "Num threads,array size,x, segment count: ";
-	std::cin >> k >> n >> x >> m;
-	int* a = new int[n]; // взятки
+int main() {
+	srand(time(0)); // рандом
+	setlocale(LC_ALL, "Russian");
+	int thread_amount, n, x, segment_amount; 
+	cout << "Num threads,array size,x, segment count: ";
+	cin >> thread_amount >> n >> x >> segment_amount;
+	int* a = new int[n]; // взятки (в реальном мире это "сумма сделки")
 	int* b = new int[n]; // откаты
 	int* c = new int[n]; // присвоенные суммы
-	int* counter = new int[m](); // счётчик значений в промежутках
+	int* counter = new int[segment_amount](); // счётчик значений в промежутках
 
-	std::ofstream a_out("a.txt");
-	std::ofstream b_out("b.txt");
-	std::ofstream c_out("c.txt");
+	ofstream a_out("a.txt");
+	ofstream b_out("b.txt");
+	ofstream c_out("c.txt");
 
-	std::ofstream fax("fax.txt", std::ios::app);
-	std::ofstream print("print.txt", std::ios::app);
-	std::ofstream histogram("histogram.txt");
-	a[0] = x*x;
+	ofstream fax("fax.txt");
+	ofstream print("print.txt");
+	ofstream histogram("histogram.txt");
+	
+	a[0] = x * x;
 	b[0] = x;
 
-#pragma omp parallel for ordered
+#pragma omp parallel for
 	for (int i = 1; i < n; i++)
 	{
-		a[i] =i*i*exp(rand()%10);
-		b[i] = i/x + rand()%100 ;
+		b[i] = i / x + rand() % 100;
+		a[i] = b[i] * exp(rand() % 10);
+		c[i] = a[i] - b[i];
 	}
-#pragma omp parallel for
-	for (int i = 0; i < n; i++) c[i] = a[i] - b[i];
-
 	int c_min = min(c, n);
 	int c_max = max(c, n);
-	int segLenght = (c_max - c_min) / m;
-	histogram << "Количество интервалов " << m << std::endl
-		<< "диапозон значений от" << c_min << " до " << c_max << std::endl;
+	int segLength = abs((c_max - c_min) / segment_amount);
+	histogram << "Количество интервалов " << segment_amount << endl
+		      << "Диапозон значений от "		 << c_min
+			  << " до "				<< c_max	 << endl 
+			  << "Длина интервала " << segLength << endl;
 
 	for (int i = 0; i < n; i++)
 	{
@@ -101,38 +100,47 @@ int main(int argc, char* argv[]){
 	}
 	a_out.close();
 	b_out.close();
+	c_out.close();
 	int avg_perc = avgPerc(a, b, n);
-	
+
 	fax << "средний процент отката от суммы взятки : " << avg_perc
-		<< std::endl << "число служащих : " << (k> n ? n : k) << std::endl;
+		<< endl << "число служащих : " << (thread_amount > n ? n : thread_amount) << endl;
 #pragma omp parallel num_threads(k> n ? n: k) shared(avg_perc)
 	{
 		int segSize = ceil(n / static_cast<int>(omp_get_num_threads()));
 		int begin = segSize * omp_get_thread_num();
 		int end = begin + segSize > n ? n : begin + segSize;
-		if(omp_get_thread_num() == 0)
+		if (omp_get_thread_num() == 0)
 		{
 			begin = 0;
 			end = segSize;
 		}
-		for(int i = begin; i < end; i++){
-			if((100 * b[i] / a[i]) > avg_perc) 
+		for (int i = begin; i < end; i++)
+		{
+			if ((100 * b[i] / a[i]) > avg_perc)
 #pragma  omp critical
+			{ fax << "служащий " << omp_get_thread_num() << " сообщение " << i << " сумма " << b[i] << endl; }
+			thread_amount = (i == 0) ? 1 : (i == n) ? n - 1 : i;
+			if (a[thread_amount] > a[i - 1] && a[thread_amount] < a[thread_amount + 1])
+#pragma  omp critical
+			{ print << "служащий " << omp_get_thread_num() << " сообщение " << thread_amount << " сумма " << a[thread_amount] << endl; }
+			for (int j = 0; j < segment_amount; j++)
 			{
-				fax << "служащий " << omp_get_thread_num() << " сообщение " << i << " сумма " << b[i] << std::endl;
-			}
-			k = (i == 0) ? 1 : (i == n) ? n - 1 : i;
-			if (a[k] > a[i - 1] && a[k] < a[k + 1])
-#pragma  omp critical
-			{ print << "служащий " << omp_get_thread_num() << " сообщение " << k << " сумма " << a[k] <<std::endl; }
-			for (int j = 0; j < m; j++)
-				if (c[i] >= c_min + (j * segLenght) && c[i] <= c_min + (j+1 * segLenght))
+				int segEnd = segment_amount - 1 == j ? c_max : c_min + (j + 1) * segLength;
+				if (c[i] >= c_min + (j * segLength) && c[i] <= + segEnd)
 #pragma omp atomic
 					counter[j]++;
+			}
 		}
 	}
-	for(int j = 0; j < m; j++)	
-		histogram << (j * segLenght) << " - " << (j + 1 * segLenght) << "   " << counter[j] << std::endl;
+	fax.close();
+	print.close();
+	for (int j = 0; j < segment_amount; j++)
+	{
+		int segEnd = segment_amount - 1 == j ? c_max : c_min + (j + 1) * segLength;
+		histogram << c_min + j * segLength << " – " << segEnd << "   " << counter[j] << endl;
+	}
+	histogram.close();
 	delete[] a;
 	delete[] b;
 	delete[] c;
